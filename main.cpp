051645ca6525d387f94b83a68797f375bc846d46
411748 <411748@bsd48.org>
2024-03-19 20:34:00 -0700

    added user input and nums file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -109,35 +111,82 @@ public:
     cout << "Binary Search Tree:" << endl;
     printTree(root);
   }
+
+  void addFromFile() {
+    int numCount;
+    cout << "Enter the number of numbers you want to add from file: ";
+    cin >> numCount;
+    ifstream file("nums.txt");
+    int num;
+    for (int i = 0; i < numCount && file >> num; ++i) {
+      insert(num);
+    }
+    file.close();
+  }
+
+  void addNumbersManually() {
+    int numCount;
+    cout << "Enter the number of numbers you want to add manually: ";
+    cin >> numCount;
+    cout << "Enter " << numCount << " numbers separated by spaces: ";
+    for (int i = 0; i < numCount; ++i) {
+      int num;
+      cin >> num;
+      insert(num);
+    }
+  }
+
+  void deleteNumber() {
+    int value;
+    cout << "Enter number to delete: ";
+    cin >> value;
+    remove(value);
+  }
+
+  void searchNumber() {
+    int value;
+    cout << "Enter number to search: ";
+    cin >> value;
+    if (search(root, value)) {
+      cout << "Number " << value << " is found in the tree." << endl;
+    } else {
+      cout << "Number " << value << " is not found in the tree." << endl;
+    }
+  }
 };
 
 int main() {
   BinarySearchTree tree;
-  tree.insert(50);
-  tree.insert(30);
-  tree.insert(70);
-  tree.insert(20);
-  tree.insert(40);
-  tree.printTree();
+  int running = 1;
+  char choice[3];
 
-  int searchValue = 30;
-  if (tree.contains(searchValue)) {
-    cout << "Searching for " << searchValue << ": Found" << endl;
-  } else {
-    cout << "Searching for " << searchValue << ": Not Found" << endl;
+  while (running) {
+    cout << "Select an option:" << endl;
+    cout << "A: Add numbers manually" << endl;
+    cout << "AF: Add numbers from file" << endl;
+    cout << "P: Print tree" << endl;
+    cout << "D: Delete number" << endl;
+    cout << "S: Search for number" << endl;
+    cout << "Q: Quit" << endl;
+    cout << "Enter choice: " << endl;
+    cin >> choice;
+
+    if (strcmp(choice, "A") == 0) {
+      tree.addNumbersManually();
+    } else if (strcmp(choice, "AF") == 0) {
+      tree.addFromFile();
+    } else if (strcmp(choice, "P") == 0) {
+      tree.printTree();
+    } else if (strcmp(choice, "D") == 0) {
+      tree.deleteNumber();
+    } else if (strcmp(choice, "S") == 0) {
+      tree.searchNumber();
+    } else if (strcmp(choice, "Q") == 0) {
+      running = 0;
+    } else {
+      cout << "Invalid choice. Please try again." << endl;
+    }
   }
-
-  searchValue = 60;
-  if (tree.contains(searchValue)) {
-    cout << "Searching for " << searchValue << ": Found" << endl;
-  } else {
-    cout << "Searching for " << searchValue << ": Not Found" << endl;
-  }
-
-  int removeValue = 30;
-  tree.remove(removeValue);
-  cout << "After removing " << removeValue << ":" << endl;
-  tree.printTree();
 
   return 0;
 }
